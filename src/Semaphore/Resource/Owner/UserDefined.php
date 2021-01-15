0eee68baf77504a5b38f27bb1dde95dffda8813d
mucha55 <przemyslaw.mucha@55places.com>
2021-01-15 01:17:09 -0600

    KOJO-185 | Cleaning up new resource owner logic

--- src/Semaphore/Resource/Owner/UserDefined.php
+++ src/Semaphore/Resource/Owner/UserDefined.php
@@ -9,7 +9,6 @@ class UserDefined implements UserDefinedInterface
 {
     use Defensive\AwareTrait;
     const PROP_RESOURCE_NAME = 'resource_name';
-    protected $resource_name_space;
     protected $resource_path;
 
     public function getResourceName(): string
@@ -37,7 +36,7 @@ class UserDefined implements UserDefinedInterface
         return false;
     }
 
-    public function setResourcePath(string $resourcePath): JobInterface
+    public function setResourcePath(string $resourcePath): UserDefinedInterface
     {
         if ($this->resource_path === null) {
             $this->resource_path = $resourcePath;
@@ -48,32 +47,12 @@ class UserDefined implements UserDefinedInterface
         return $this;
     }
 
-    protected function getResourcePath(): string
+    public function getResourcePath(): string
     {
         if ($this->resource_path === null) {
             throw new \LogicException('Resource path is not set.');
         }
 
         return $this->resource_path;
-    }
-
-    public function setResourceNameSpace(string $resource_name_space): JobInterface
-    {
-        if ($this->resource_name_space === null) {
-            $this->resource_name_space = $resource_name_space;
-        } else {
-            throw new \LogicException('Resource name space is already set.');
-        }
-
-        return $this;
-    }
-
-    protected function getResourceNameSpace(): string
-    {
-        if ($this->resource_name_space === null) {
-            throw new \LogicException('Resource name space is not set.');
-        }
-
-        return $this->resource_name_space;
     }
 }
